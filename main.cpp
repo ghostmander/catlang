d9e90217d1cafb8d944826107a214674e8a83a39
ghostmander <jatinjindal.2003@gmail.com>
2025-03-07 13:43:32 +0530

    feat: Infix to Postfix conversion!
    - Now the mathematical expressions are written in infix formatting instead of postfix
     -- Will add an ability to do both sometime later (?)

--- main.cpp
+++ main.cpp
@@ -7,13 +7,56 @@
 
 using namespace std;
 
+vector<string> infix_to_rpn(const string& infix) {
+    stack<string> st;
+    vector<string> output;
+    unordered_map<string, pair<int, string>> opPrec = {
+            { "+", make_pair(0, "L") },  // ADDITION
+            { "-", make_pair(0, "L") },  // SUBTRACTION
+            { "/", make_pair(5, "L") },  // DIVISION
+            { "*", make_pair(5, "L") },  // MULTIPLICATION
+            { "%", make_pair(5, "L") },  // MODULUS
+            { "^", make_pair(10, "R") }, // EXPONENT
+    };
+    for (const auto &token: split(infix)) {
+        if (opPrec.count(token) != 0) {
+            while (!st.empty() && opPrec.count(st.top()) != 0 &&
+                   ((opPrec[token].second == "L" && opPrec[token].first <= opPrec[st.top()].first) ||
+                    (opPrec[token].second == "R" && opPrec[token].first < opPrec[st.top()].first))) {
+                output.push_back(st.top());
+                st.pop();
+            }
+            st.push(token);
+        }
+        else if (token == "(")
+            st.push(token);
+        else if (token == ")") {
+            while (!st.empty() && st.top() != "(") {
+                output.push_back(st.top());
+                st.pop();
+            }
+            st.pop();
+        }
+        else
+            output.push_back(token);
+    }
+    while (!st.empty()) {
+        output.push_back(st.top());
+        st.pop();
+    }
+    return output;
+}
+
+
 class Evaluator {
     unordered_map<string, int> variable_map = {};
 
     int calculate_expression(const string &expr) {
         stack<int> st;
         int lhs, rhs;
-        for (const auto &token: split(expr)) {
+
+
+        for (const auto &token: infix_to_rpn(expr)) {
             if (isDigit(token))
                 st.push(stoi(token));
             else if (variable_map.count(token))
@@ -66,12 +109,18 @@ public:
     }
 };
 
+
+
+
 int main(int argc, char *argv[]) {
     if (argc < 2) {
         cerr << "No file path found." << endl << "Usage: " << argv[0] << "<path-to-program-file>";
         exit(-1);
     }
     vector<string> lines = read_file(argv[1]);
+//    for (const auto& tok : infix_to_rpn("A ^ 2 + 2 * A * B + B ^ 2"))
+//        cout << tok << ' ';
+//    cout << endl;
     Evaluator evaluator;
     evaluator.evaluate_program(lines);
     // Print the lines to verify
